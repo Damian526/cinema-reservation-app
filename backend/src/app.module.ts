7f730ce3a82b3f15fb900705386b896e690e50f0
Damian526 <damianzieba999@gmail.com>
2025-06-15 12:04:19 +0200

    set up db

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3307, // Twoje mapowanie 3307:3306
+      username: 'kino_user',
+      password: 'UserPass123',
+      database: 'kino',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      migrations: [__dirname + '/migrations/*{.ts,.js}'],
+      synchronize: false, // migracje zamiast auto-schematu
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
